1598b42541c05361ed86916ac98c7f56274579a9
Juan Manuel Bolivar Mojica <jbolivarm2@ucentral.edu.co>
2025-11-21 09:37:28 -0500

    configuracion Camara al jugador

--- TriLab/Assets/script/CamaraControlador.cs
+++ TriLab/Assets/script/CamaraControlador.cs
@@ -1,19 +1,41 @@
 using UnityEngine;
-using System.Collections;
-public class ControladorCamara:MonoBehaviour
+using UnityEngine.InputSystem;
+
+public class ControladorCamara : MonoBehaviour
 {
     public GameObject jugador;
     public Vector3 _distanciaCamara;
-    
+    public float sensibilidad = 200f;
+
+    private Vector2 lookInput;
+    private float rotacionX = 0f;
+
     void Start()
     {
-        _distanciaCamara = transform.position- jugador.transform.position;
+        _distanciaCamara = transform.position - jugador.transform.position;
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
+
     void Update()
     {
-        transform.position = jugador.transform.position +_distanciaCamara;
+        float mouseX = lookInput.x * sensibilidad * Time.deltaTime;
+        float mouseY = lookInput.y * sensibilidad * Time.deltaTime;
+
+        rotacionX -= mouseY;
+        rotacionX = Mathf.Clamp(rotacionX, -80f, 80f);
+
+        transform.localRotation = Quaternion.Euler(rotacionX, 0f, 0f);
+
+        jugador.transform.Rotate(Vector3.up * mouseX);
+
+        transform.position = jugador.transform.position + _distanciaCamara;
     }
 
+    // ESTE MÉTODO LO LLAMA EL INPUT SYSTEM AUTOMÁTICAMENTE
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        lookInput = context.ReadValue<Vector2>();
+    }
 }
-
-
