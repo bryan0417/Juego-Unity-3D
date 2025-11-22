f1ee2b4a6e9f2e395a8f66515c81b8472ebdd23f
Juan Manuel Bolivar Mojica <jbolivarm2@ucentral.edu.co>
2025-11-22 18:37:34 -0500

    Implementacion animimacion personaje Juan

---
+++ TriLab/Assets/script/logica_personaje_n3.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class logica_personaje_n3 : MonoBehaviour
+{
+    public float velocidadMovimiento = 5f;
+    public float velocidadRotacion = 200f;
+
+    private Animator ani;
+    private float x, y;
+
+    void Start()
+    {
+        ani = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        x = Input.GetAxis("Horizontal");
+        y = Input.GetAxis("Vertical");
+
+        // Rotación
+        transform.Rotate(0, x * Time.deltaTime * velocidadRotacion, 0);
+
+        // Movimiento (espacio LOCAL)
+        Vector3 movimiento = transform.forward * y * velocidadMovimiento * Time.deltaTime;
+        transform.position += movimiento;
+
+        // Animación
+        ani.SetFloat("VelX", x);
+        ani.SetFloat("VelY", y);
+    }
+}
