e34672ea967de4287422c457486d7374a1c491cb
CarolineDiaz29 <CarolineDiaz29>
2025-11-26 17:40:35 -0500

    Arreglo puzzle

--- TriLab/Assets/script/Puzzle/Rotar piezas.cs
+++ TriLab/Assets/script/Puzzle/Rotar piezas.cs
@@ -5,7 +5,7 @@ public class Rotarpiezas : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        int[] angulos = { 0, 90, 180, 270 };
+        int[] angulos = {90, 180, 270 };
         foreach (Transform pieza in transform)
         {
             int randomIndex = Random.Range(0, angulos.Length);
